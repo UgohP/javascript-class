87fe9c0da22c6ece0634f412afca43e7bf1db241
Paschal Ugoh <paskyugoh@gmail.com>
2025-09-04 23:22:13 +0100

    finished the online class

---
+++ semptember_class/example/eg.js
@@ -0,0 +1,6 @@
+const firstName = "John";
+const lastName = "Doe";
+const doe = document.getElementById("doe");
+
+doe.textContent = `Hello, My name is ${firstName} ${lastName}`;
+doe.style.color = "black";
