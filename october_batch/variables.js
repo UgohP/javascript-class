4792e1fedab389b016a3f4f4a1b0c989bc73cbdb
Paschal Ugoh <paskyugoh@gmail.com>
2025-10-03 11:13:52 +0100

    done with this week javascript class

---
+++ october_batch/variables.js
@@ -0,0 +1,8 @@
+const firstname = "John";
+const lastname = "Doe";
+const age = 15;
+const isStudent = true;
+
+console.log("My name is " + firstname); //string concatination
+
+console.log(`My name is ${firstname} ${lastname} and I am ${age} years old`);
