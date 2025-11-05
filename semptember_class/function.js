cdf7c35410023fa25f7f2a845c71bc1b10f652a5
Paschal Ugoh <paskyugoh@gmail.com>
2025-11-05 17:13:58 +0100

    did a little javascript beginner tutorial

---
+++ semptember_class/function.js
@@ -0,0 +1,14 @@
+// function Greeting1(name) {
+//   return `Hello ${name}`;
+// }
+
+// const Greeting2 = (name, age) => {
+//   return `Hello ${name}, You are ${age} years old`;
+// };
+
+// console.log(Greeting1("John"), Greeting2("John", 29));
+// console.log(Greeting2("Mark", 16));
+
+const fruits = ["pineapple", "apple", "orange", "mango"];
+
+fruits.map((fruit) => console.log(fruit));
